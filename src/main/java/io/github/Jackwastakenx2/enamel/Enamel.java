13c6862131e45d62e636dca48940cdc8e97c1078
Jackwastakenx2 <88010263+jackwastakenx2@users.noreply.github.com>
2026-04-09 12:01:55 -0400

    Initial commit

---
+++ src/main/java/io/github/Jackwastakenx2/enamel/Enamel.java
@@ -0,0 +1,15 @@
+package io.github.Jackwastakenx2.enamel;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Enamel implements ModInitializer {
+	public static final String ID = "enamel";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+	@Override
+	public void onInitialize() {
+		//LOGGER.info("[Mod ID] pretty pink princess ponies prancing perpendicular");
+	}
+}
